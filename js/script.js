d9b4ec3c32eeaecb1d5be4329c79cf69f7ec54f0
przemek3d <90689584+przemek3d@users.noreply.github.com>
2021-12-03 21:27:28 +0100

    Final update

--- js/script.js
+++ js/script.js
@@ -1,16 +1,64 @@
 const menu = document.querySelector(".menu");
-let support_value = JSON.parse(data);
-let value = support_value[0].value;
+//let support_value = JSON.parse(data);
+//let value = support_value[0].value;
+let value = Math.floor(Math.random() * 1000 + 5000);
 let section = document.querySelector("#section_1");
 let section2 = document.querySelector("#section_2");
 let h = section.clientHeight;
 let h2 = section2.clientHeight;
 let session = 0;
 
+if (session == 0) {
+  let loading = document.createElement("div");
+  let circle = document.createElement("div");
+  document.body.appendChild(loading);
+  loading.appendChild(circle);
+  loading.className = "landing_animation";
+  loading.style.position = "absolute";
+  circle.innerHTML = "<h1>PTVS</h1>";
+  circle.style.fontFamily = "var(--font)";
+  circle.style.fontSize = "8vh";
+  circle.id = "circle";
+  circle.style.backgroundColor = "var(--color_pink)";
+  circle.style.borderRadius = "100vw";
+  circle.style.color = "var(--color_white)";
+  loading.style.zIndex = "999";
+  loading.style.top = "0";
+  loading.style.bottom = "0";
+  loading.style.left = "0";
+  loading.style.right = "0";
+  loading.style.display = "flex";
+  loading.style.backgroundColor = "var(--color_white)";
+  loading.style.justifyContent = "center";
+  loading.style.alignItems = "center";
+  setTimeout(() => {
+    loading.style.animation = "puff 1s ease-in-out";
+  }, 1000);
+  setTimeout(() => {
+    loading.style.display = "none";
+    document.body.style.animation = "fade 1s ease";
+  }, 1000);
+  document.body.style.animation = "none";
+}
+
 function sticky() {
   menu.classList.add("sticky", window.scrollY >= 1);
-  console.log(window.scrollY);
   if (scrollY > h + h2) {
+    menu.classList.remove("sticky");
+  }
+  if (scrollY > 2 * h - 100) {
+    menu.classList.remove("sticky");
+  }
+  if (scrollY < 2 * (h + h2) - 100) {
+    menu.classList.add("sticky");
+  }
+  if (scrollY > h + h2 - 100 && scroll < 3 * h2) {
+    menu.classList.remove("sticky");
+  }
+  if (scrollY > 2 * h - 100 && scrollY < 3 * h - 100) {
+    menu.classList.remove("sticky");
+  }
+  if (scroll < 3 * h2) {
     menu.classList.remove("sticky");
   }
   if (window.scrollY == 0) {
