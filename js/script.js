032095c8dab57fcc35ca2781b62c0c9dafaac3e5
przemek3d <90689584+przemek3d@users.noreply.github.com>
2021-12-03 15:57:14 +0100

    Progress, adapted styling

--- js/script.js
+++ js/script.js
@@ -1,41 +1,51 @@
 const menu = document.querySelector(".menu");
 let support_value = JSON.parse(data);
-let value = (support_value[0].value);
+let value = support_value[0].value;
+let section = document.querySelector("#section_1");
+let section2 = document.querySelector("#section_2");
+let h = section.clientHeight;
+let h2 = section2.clientHeight;
 let session = 0;
 
-function sticky(){
-    menu.classList.toggle ("sticky", window.scrollY > 0)
+function sticky() {
+  menu.classList.add("sticky", window.scrollY >= 1);
+  console.log(window.scrollY);
+  if (scrollY > h + h2) {
+    menu.classList.remove("sticky");
+  }
+  if (window.scrollY == 0) {
+    menu.classList.remove("sticky");
+  }
 }
 
-function count(){
-    const counters = document.querySelectorAll(".text_amount");
-    let speed = 50;
+function count() {
+  const counters = document.querySelectorAll(".text_amount");
+  let speed = 25;
 
-    if(session == 1){
-        return;
-    }
+  if (session == 1) {
+    return;
+  }
 
-    setTimeout(() => {
-        counters.forEach(counter => {
-            const update_counter = () => {
-                const count = +counter.innerText;
-                const velo = (Math.floor(value / speed));
-    
-                if (count < value) {
-                    speed += 3;
-                    counter.innerText = count + velo;
-                    setTimeout(update_counter, 1);
-                } 
-                else {
-                    counter.innerText = value;
-                }
-            };
-    
-            update_counter();
-        });
-    }, 500);
-    session++;
+  setTimeout(() => {
+    counters.forEach((counter) => {
+      const update_counter = () => {
+        const count = +counter.innerText;
+        const velo = Math.floor(value / speed);
+
+        if (count < value) {
+          speed += 3;
+          counter.innerText = count + velo;
+          setTimeout(update_counter, 1);
+        } else {
+          counter.innerText = value;
+        }
+      };
+
+      update_counter();
+    });
+  }, 500);
+  session++;
 }
 
-window.addEventListener("scroll", sticky);
 window.addEventListener("scroll", count);
+window.addEventListener("scroll", sticky);
